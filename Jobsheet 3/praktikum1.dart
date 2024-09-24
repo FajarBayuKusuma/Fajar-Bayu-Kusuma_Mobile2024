14a61fd84662cbb9b0fefebddc52a566e178e3e1
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2024-09-24 23:20:45 +0700

    Penyesuaian

--- Jobsheet 3/praktikum1.dart
+++ Jobsheet 3/praktikum1.dart
@@ -1,10 +1,14 @@
-String test = "test2";
-if (test == "test1") {
-   print("Test1");
-} else If (test == "test2") {
-   print("Test2");
-} Else {
-   print("Something else");
-}
+void main(){
+      String test = "test2";
+    if (test == "test1") {
+      print("Test1");
+    } 
+    else if (test == "test2") {
+      print("Test2");
+    } 
+    else {
+      print("Something else");
+    }
 
-if (test == "test2") print("Test2 again");
+    if (test == "test2") print("Test2 again");
+}
