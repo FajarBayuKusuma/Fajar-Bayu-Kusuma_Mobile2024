5d3e5cdcd127c98b269fd67916a9e02eb30ab3c6
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2024-09-27 09:33:35 +0700

    Perbaikan README

--- Jobsheet 4/praktikum2.dart
+++ Jobsheet 4/praktikum2.dart
@@ -2,11 +2,19 @@ void main(List<String> args) {
   var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
   print(halogens);
   
-  var names1 = <String>{};
-  Set<String> names2 = {}; // This works, too.
-  var names3 = {}; // Creates a map, not a set.
+  // var names1 = <String>{};
+  // Set<String> names2 = {}; // This works, too.
+  // var names3 = {}; // Creates a map, not a set.
 
-  print(names1);
-  print(names2);
-  print(names3);
+  // print(names1);
+  // print(names2);
+  // print(names3);
+
+  var names1 = <String>{};
+Set<String> names2 = {};
+names1.add('Fajar Bayu Kusuma');
+names2.add('NIM 2241720085');
+names1.addAll(halogens); // Pastikan 'halogens' sudah didefinisikan sebelumnya.
+print(names1);
+print(names2);
 }
