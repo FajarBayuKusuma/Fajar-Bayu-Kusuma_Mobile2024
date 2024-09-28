908085a7f3826d8d76e01dd8b96d1f60f8190a4b
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2024-09-29 01:07:53 +0700

    Perbaikan

--- Jobsheet 4/praktikum4.dart
+++ Jobsheet 4/praktikum4.dart
@@ -16,7 +16,23 @@ void main(List<String> args) {
   print(list4);
 
   //Langkah 4
-  var promoActive = false;
+  var promoActive = true;
   var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
   print(nav);
+
+  //Langkah 5
+  String login = 'Manager';
+  var nav2 = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (login case 'Manager') 'Inventory'
+    ];
+    print(nav2);
+
+    //Langkah 6
+    var listOfInts = [1, 2, 3];
+    var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+    assert(listOfStrings[1] == '#1');
+    print(listOfStrings);
 }
