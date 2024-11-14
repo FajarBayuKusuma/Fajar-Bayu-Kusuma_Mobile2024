2456ec2ca90e6b689e3fbf7a39423b2f29003964
FajarBayuKusuma <114029998+FajarBayuKusuma@users.noreply.github.com>
2024-11-14 21:05:30 +0700

    Praktikum 1

--- Jobsheet 9 - Plugin Kamera/photo_filter_carousel/lib/main.dart
+++ Jobsheet 9 - Plugin Kamera/photo_filter_carousel/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_filter_carousel/takepicture_screen.dart';
 import 'package:photo_filter_carousel/widget/filter_carousel.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 Future<void> main() async {
   // Pastikan bahwa plugin sudah diinisialisasi
+  
   WidgetsFlutterBinding.ensureInitialized();
 
   // Dapatkan daftar kamera yang tersedia di perangkat
@@ -12,6 +14,10 @@ Future<void> main() async {
 
   // Ambil kamera pertama dari daftar kamera
   final firstCamera = cameras.first;
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOption.currentPlatform,
+);
 
   // Jalankan aplikasi Flutter
   runApp(
@@ -20,4 +26,8 @@ Future<void> main() async {
       debugShowCheckedModeBanner: false,
     ),
   );
+}
+
+class DefaultFirebaseOption {
+  static var currentPlatform;
 }
